b2b143017418424a156e3a589e9eaaad1e2ccbc7
mudit <mudit.chauhan@tcs.com>
2021-04-19 11:49:37 +0530

    updated file

--- test.tf
+++ test.tf
@@ -1,15 +1,5 @@
 resource "null_resource" "test" {
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("C:\\Users\\mudit\\Downloads\\ec2-creds.pem")
-    host     = "13.233.122.152"
-	port 	 = 22
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "mkdir test",
-    ]
+  provisioner "local-exec" {
+    command = "echo hi >> C:\\Users\\mudit\\Desktop\\GIT\\test.txt"
   }
 }
